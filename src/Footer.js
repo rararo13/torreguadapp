dfeeff70d43b2ac4b9bef3e79448878870dea112
Rafa <rafael.ramirez@babelgroup.com>
2023-10-31 17:28:17 +0100

    añado links provisionales a los botones del footer

--- src/Footer.js
+++ src/Footer.js
@@ -11,6 +11,31 @@ import {
 } from 'mdb-react-ui-kit';
 
 export default function Footer() {
+
+  const handleEmailButtonClick = () => {
+    const destinatario = 'cyftorreguada@gmail.com';
+    const mailtoLink = `mailto:${destinatario}`;
+    window.location.href = mailtoLink;
+  };
+
+ const redirectToFacebookProfile =
+  () => {
+      const facebookProfileURL = `https://www.facebook.com/torreguada.cubiertasyfachadas`;
+      window.location.href = facebookProfileURL;
+    };
+
+  const redirectToInstagramProfile =
+  () => {
+      const instagramProfileURL = `https://www.instagram.com/juanantoniojimenezz/`;
+      window.location.href = instagramProfileURL;
+    };
+
+    const redirectToLinkedinProfile =
+    () => {
+        const linkedinProfileURL = `https://www.linkedin.com/in/rafael-ramírez-román-691701223/`;
+        window.location.href = linkedinProfileURL;
+      };
+ 
   return (
     <MDBFooter className='bg-light text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
@@ -19,27 +44,20 @@ export default function Footer() {
             floating
             className='m-1'
             style={{ backgroundColor: '#3b5998' }}
+            onClick={redirectToFacebookProfile}
             href='#!'
             role='button'
           >
             <MDBIcon fab icon='facebook-f' />
           </MDBBtn>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#55acee' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='twitter' />
-          </MDBBtn>
 
           <MDBBtn
             floating
             className='m-1'
             style={{ backgroundColor: '#dd4b39' }}
             href='#!'
+            onClick={handleEmailButtonClick}
             role='button'
           >
             <MDBIcon fab icon='google' />
@@ -49,6 +67,7 @@ export default function Footer() {
             className='m-1'
             style={{ backgroundColor: '#ac2bac' }}
             href='#!'
+            onClick={redirectToInstagramProfile}
             role='button'
           >
             <MDBIcon fab icon='instagram' />
@@ -58,21 +77,13 @@ export default function Footer() {
             floating
             className='m-1'
             style={{ backgroundColor: '#0082ca' }}
+            onClick={redirectToLinkedinProfile}
             href='#!'
             role='button'
           >
             <MDBIcon fab icon='linkedin-in' />
           </MDBBtn>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#333333' }}
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab icon='github' />
-          </MDBBtn>
         </section>
       </MDBContainer>
 
@@ -85,3 +96,4 @@ export default function Footer() {
     </MDBFooter>
   );
 }
+
